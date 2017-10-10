9c22510fd695575713370cf30403bad88ee8bf5d
orsi1917 <orsi1917@yahoo.co.uk>
2017-10-10 17:17:56 +0200

    table

--- src/main/java/nl/youngcapital/LabJournal/rest/ProjectEindpoint.java
+++ src/main/java/nl/youngcapital/LabJournal/rest/ProjectEindpoint.java
@@ -3,12 +3,15 @@ package nl.youngcapital.LabJournal.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import nl.youngcapital.LabJournal.Project;
 import nl.youngcapital.LabJournal.Controller.ProjectRepository;
@@ -40,8 +43,13 @@ public class ProjectEindpoint {
 		System.out.println(project.getName());
 		projectService.test(project);
 	}
-	  @RequestMapping(value = "/list", method = RequestMethod.GET)
-	  public List<Project> findAll() {
+	 @RequestMapping(value = "/list", method = RequestMethod.GET)
+	 public List<Project> findAll() {
 	    return (List<Project>)projectRepository.findAll();
 	  }
+	  @ResponseStatus(value = HttpStatus.OK)
+	  @RequestMapping(value = "/project/{id}", method = RequestMethod.DELETE)
+	  public void updateProject(@PathVariable  long id) {
+	    projectRepository.delete(id);
+	  } 
 }
