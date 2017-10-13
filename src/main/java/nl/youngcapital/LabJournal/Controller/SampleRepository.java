0c34b236040822a05c82ffef8dd8e864a575c713
orsi1917 <orsi1917@yahoo.co.uk>
2017-10-13 15:36:55 +0200

    filter version 5

--- src/main/java/nl/youngcapital/LabJournal/Controller/SampleRepository.java
+++ src/main/java/nl/youngcapital/LabJournal/Controller/SampleRepository.java
@@ -12,15 +12,12 @@ import nl.youngcapital.LabJournal.Sample;
 
 @Component
 public interface SampleRepository extends CrudRepository<Sample, Long>{
-	@Transactional
-  	@Modifying
+	
 //	@Query("UPDATE Game SET high_score = ? where ? > high_score and type_game = ?")
 //  	"SELECT id, title FROM todos t WHERE " +
 //    "LOWER(t.title) LIKE LOWER(CONCAT('%',:searchTerm, '%')) OR " +
 //    "LOWER(t.description) LIKE LOWER(CONCAT('%',:searchTerm, '%')) " +
 //    "ORDER BY t.title ASC"
-  	@Query("SELECT id, name, project FROM Sample s WHERE s.name=?")
-	public List<Sample> filtersamples(String name);
 
 	@Transactional
   	@Modifying
