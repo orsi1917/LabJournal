662fde068b9ec5291e93fe2c6fdbb56904c33163
orsi1917 <orsi1917@yahoo.co.uk>
2017-10-19 13:36:29 +0200

    textarea

--- src/main/resources/templates/LabJournal.js
+++ src/main/resources/templates/LabJournal.js
@@ -578,6 +578,16 @@ function RadioEnabler() {
 // 	function handleExperimentSelector(idpass) {
 // 		document.getElementById("deleteExamplefield").value = idpass;
 // 	}
+	function getQueryVariable(variable)
+	{
+	       var query = window.location.search.substring(1);
+	       var vars = query.split("&");
+	       for (var i=0;i<vars.length;i++) {
+	               var pair = vars[i].split("=");
+	               if(pair[0] == variable){return pair[1];}
+	       }
+	       return(false);
+	}
 
 	/* ---------------------------------------- Request Handling -------------------------------------------------
 	--------------------------------------------------------------------------------------------------------- */
@@ -614,6 +624,6 @@ function RadioEnabler() {
 	ListExperiment();
 	ListSubSamples();
 	}
-	dataLoader();
+	
 
 	
